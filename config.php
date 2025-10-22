981bdc67f8089fa2f2a06f6ad3cb5ff9c58f10ec
Gonzalo <gonzalomartinez293@hotmail.com>
2025-10-22 00:37:54 -0300

    actualizo

--- config.php
+++ config.php
@@ -2,4 +2,8 @@
     define ("DB_NAME" ,  "tpe_web_2_2025");
     define ("DB_USER_NAME" ,  "root");
     define ("DB_PASSWORD" ,  "");
+
+    const MYSQL_USER = 'root';
+    const MYSQL_PASS = '';
+    const MYSQL_DB = 'dbtasks';
     const MYSQL_HOST = 'localhost';
